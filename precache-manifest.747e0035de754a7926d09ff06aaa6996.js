0eff39b0f4b51540d44c0e11388def8dc5ca5607
Rajkiran <rajkiran@happyfox.com>
2019-08-31 16:00:41 +0530

    Updates

---
+++ precache-manifest.747e0035de754a7926d09ff06aaa6996.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b9ae9826c99cc15177055b02566748",
+    "url": "/React-Basics/index.html"
+  },
+  {
+    "revision": "7f3e84ca71e1bd930616",
+    "url": "/React-Basics/static/css/main.12a358e2.chunk.css"
+  },
+  {
+    "revision": "8c3cc99f5b667d05a44f",
+    "url": "/React-Basics/static/js/2.f29d4b71.chunk.js"
+  },
+  {
+    "revision": "7f3e84ca71e1bd930616",
+    "url": "/React-Basics/static/js/main.b10e89fc.chunk.js"
+  },
+  {
+    "revision": "c5f7db13e38d751b096a",
+    "url": "/React-Basics/static/js/runtime~main.9361e79e.js"
+  }
+]);
